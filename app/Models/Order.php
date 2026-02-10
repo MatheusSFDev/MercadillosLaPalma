00fea7e1eda1e503c25fecd86efb33d9782ef3f7
brianpr <brian23perezperez@gmail.com>
2026-02-10 18:49:44 +0000

    Hecho User y Order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Product;
+use App\Models\User;
+use App\Models\Stall;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'home_delivery',
+        'information',
+        'active',
+        'reset_date',
+    ];
+
+    // Falta test relaciones
+    public function product(): HasMany {
+        return $this->hasMany(Product::class);
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function stall(): BelongsTo {
+        return $this->belongsTo(Stall::class);
+    }
 }
