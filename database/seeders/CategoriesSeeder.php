18e9a8dcc0c6b491392e11562a1f65a19663c889
ElZorroGG <106108663+ElZorroGG@users.noreply.github.com>
2026-02-10 17:13:03 +0000

    SeedersPT1

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $Categories=[
+            'Carniceria',
+            'Pescaderia',
+            'Verduleria',
+            'Artesania',
+            'Otros',
+        ];
+        foreach($Categories as $Category){
+            DB::table('categories')->insert([
+                'name' => $Category,
+            ]);
+        }
+    }
+}
