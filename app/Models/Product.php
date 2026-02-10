d35336e384137ea98e210a0221b011006caa7365
raulcitorm <raulrodmartin@gmail.com>
2026-02-10 18:02:35 +0000

    Creacion modelos
    Product, Photo, Category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,48 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
-    //
+
+    protected $fillable = [
+        'name',
+        'photos',
+        'unit',
+    ];
+
+    public function user(): BelongsTo
+    {
+
+        return $this->belongsTo(User::class);
+
+    }
+
+    public function category(): BelongsTo
+    {
+
+        return $this->belongsTo(Category::class);
+
+    }
+
+    public function photos(): HasMany
+    {
+
+        return $this->hasMany(Photo::class);
+
+    }
+    public function orders(): BelongsToMany
+    {
+
+        return $this->belongsToMany(Order::class)->withPivot('quantity', 'price_per_unit','status');
+
+    }
+    public function stock():BelongsToMany
+    {
+        return $this->belongsToMany(Stall::class)->withPivot('quantity', 'price_per_unit');
+    }
+
 }
