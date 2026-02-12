3ae5d53ec751c9273e5bd57b87e9cec8fa96a3de
Bisferoz45 <gabriel.cardenas.martin@gmail.com>
2026-02-12 20:58:56 +0100

    Pirmeros pasos con validaciones

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -2,26 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class SellerController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Return the view 'sellers.stallSellers'
+     * @return \Illuminate\Contracts\View\View
      */
     public function indexStalls()
     {
         return view("sellers.stallSellers");
     }
+
+    /**
+     * Return the view 'sellers.addProducts'
+     * @return \Illuminate\Contracts\View\View
+     */
     public function createProduct()
     {
         return view("sellers.addProducts");
+    }
+    
+    public function storeProduct(Request $request){
+        $product_validated = $request->validate([
+            "name" => "required|string",
+            "unit" => "required|in:Kg,gr,L,mL,unidad/es",
+            "category_id"=> "required|integer",
+            "user_id" => "required|integer",
+        ]);
+
+        $stock_validation = $request->validate([
+            "product_id"=> "required|integer",
+            "stall_id" => "required|integer",
+            "quantity" => "required|integer",
+            "price_per_unit" => "required|numeric|decimal:0,2",
+        ]);
+
+        
+
+        $product = Product::create();
+
+        return response()->json($product);
     }
 
     public function editProducts()
     {
         return view("sellers.editProducts");
     }
+
     /**
      * Show the form for creating a new resource.
      */
