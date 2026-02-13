9f71e691254af2dacfd24f3fcdf16e7834c032fc
ElZorroGG <106108663+ElZorroGG@users.noreply.github.com>
2026-02-13 18:44:50 +0000

    Update (Dudas)

--- tests/Feature/DB/DBTest.php
+++ tests/Feature/DB/DBTest.php
@@ -2,13 +2,10 @@
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Municipality;
-/*
-Permite hacer Pruebas en la base de datos, se encarga de limpiar la base de datos después de cada prueba, para evitar que los datos de una prueba afecten a otra prueba.
-*/ 
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\assertDatabaseMissing;
-use function Pest\Laravel\assertAuthenticated;
-use function Pest\Laravel\post;
 uses(RefreshDatabase::class);
 /*Test que crea un usuario y verifica que se haya introductido en la base de datos*/ 
 
@@ -85,4 +82,84 @@ test("CRUD Municipios", function (){
     // Verificar eliminación
     assertDatabaseMissing('municipalities', [
         'id' => $municipality->id,    ]);
+});
+test("CRUD Categorias", function () {
+    DB::table('categories')->insert([
+        'name' => 'Frutas',
+    ]);
+    // Verificar creación
+    assertDatabaseHas('categories', [
+        'name' => 'Frutas',
+    ]);
+    // Actualizar categoría
+    DB::table('categories')
+        ->where('name', 'Frutas')
+        ->update(['name' => 'Verduras']);
+    // Verificar actualización
+    assertDatabaseHas('categories', [
+        'name' => 'Verduras',
+    ]);
+    // Verificar que el nombre anterior ya no existe
+    assertDatabaseMissing('categories', [
+        'name' => 'Frutas',
+    ]);
+    // Eliminar categoría
+    DB::table('categories')
+        ->where('name', 'Verduras')
+        ->delete();
+    // Verificar eliminación
+    assertDatabaseMissing('categories', [
+        'name' => 'Verduras',
+    ]);
+});
+test("CRUD Productos", function (){
+    DB::table('categories')->insert([
+        'name' => 'Frutas',
+    ]);
+    DB::table('users')->insert([
+        'name' => 'John',
+        'surname' => 'Doe',
+        'email' => 'Jhon.2example.com',
+        'password' => bcrypt('password'),
+    ]);
+    DB::table('products')->insert([
+        'name' => 'Producto 1',
+        'unit'=> 'kg',
+        'user_id' => DB::table('users')->where('email', 'Jhon.2example.com')->value('id'),
+        'category_id' => DB::table('categories')->where('name', 'Frutas')->value('id'),
+    ]);
+    // Verificar creación
+    assertDatabaseHas('products', [
+        'name' => 'Producto 1',
+        'unit' => 'kg',
+        'user_id' => DB::table('users')->where('email', 'Jhon.2example.com')->value('id'),
+        'category_id' => DB::table('categories')->where('name', 'Frutas')->value('id'),
+    ]);
+    // Actualizar producto
+    DB::table('products')
+        ->where('name', 'Producto 1')
+        ->update(['name' => 'Producto 1 actualizado']);
+    // Verificar actualización
+    assertDatabaseHas('products', [
+        'name' => 'Producto 1 actualizado',
+    ]);
+    // Verificar que el nombre anterior ya no existe
+    assertDatabaseMissing('products', [
+        'name' => 'Producto 1',
+    ]);
+    // Eliminar producto
+    DB::table('products')
+        ->where('name', 'Producto 1 actualizado')
+        ->delete();
+    // Verificar eliminación
+    assertDatabaseMissing('products', [
+        'name' => 'Producto 1 actualizado',
+    ]);
+    // Limpiar tablas relacionadas
+    DB::table('categories')
+        ->where('name', 'Frutas')
+        ->delete();
+    DB::table('users')
+        ->where('email', 'Jhon.2example.com')
+        ->delete();
 });
