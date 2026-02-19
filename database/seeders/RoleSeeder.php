8e256c4e0cff9e9a1c1efc12212e1e5c6a881701
sylvanor <adfor409@gmail.com>
2026-02-19 18:44:57 +0000

    Añadido el rol de root
    Que al parecer lo necesitamos ¯\_(ツ)_/¯

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -9,6 +9,7 @@ class RoleSeeder extends Seeder
 {
     public function run(): void
     {
+        Role::firstOrCreate(['name' => 'root']);
         Role::firstOrCreate(['name' => 'admin']);
         Role::firstOrCreate(['name' => 'seller']);
         Role::firstOrCreate(['name' => 'customer']);
