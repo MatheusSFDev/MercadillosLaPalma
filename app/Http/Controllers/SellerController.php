67b5f690c0285437cc076c7fbf9f3ffa05502888
Bisferoz45 <gabriel.cardenas.martin@gmail.com>
2026-02-21 15:09:16 +0100

    Funciones de controlador de vendedor terminadas. Falta revisión

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Enums\Status;
 use App\Enums\Units;
+use App\Http\Requests\StoreProductRequest;
 use App\Models\Order;
+use App\Models\Photo;
+use App\Models\Product;
 use App\Models\Stall;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -67,6 +70,102 @@ class SellerController extends Controller
             return view('general.orders', [$data]);
         }else{
             return view('/');
+        }
+    }
+
+    public function sellerStalls(){
+        if(Auth::user()->hasRole('seller')){
+            $stalls = Auth::user()->stalls();
+            $data = [];
+            foreach($stalls as $stall){
+                $total = $stall->orders()
+                            ->join('order_product', 'orders.id', '=', 'order_product.order_id')
+                            ->whereBetween('orders.order_date', [
+                                now()->startOfWeek(),
+                                now()->endOfWeek()
+                            ])
+                            ->selectRaw('SUM(order_product.price_per_unit * order_product.quantity)')
+                            ->value('SUM(order_product.price_per_unit * order_product.quantity)');
+
+                array_push($data, [
+                    "name" => $stall->name,
+                    "municipio" => $stall->fleaMarket()->municipality()->name,
+                    "category" => $stall->categries,
+                    "status" => $stall->active,
+                    "products" => $stall->products()->count(),
+                    "orders" => $stall->orders()->count(),
+                    "total" => $total,
+                ]);
+            }
+
+            return view('seller.stalls', [$data]);
+        }else{
+            return view('/');
+        }
+    }
+
+    public function sellerProducts(){
+        if(Auth::user()->hasRole('seller')){
+            $products = Auth::user()->products();
+            $data = [
+                "total_products" => Auth::user()->products()->count(),
+                "assigneds" => 0,
+                "unassigneds" => 0,
+                "active_stalls" => Auth::user()->stalls()->where('active', true)->count(),
+                "products" => []
+            ];
+
+            foreach($products as $product){
+                $status = $product->stalls()->where('product_id', $product->id)->exists();
+
+                array_push($data['products'], [
+                    "name" => $product->name,
+                    "status" => $status == false ? 'Sin asignar' : 'Asignado',
+                    "stalls" => $status == false ? 'Sin asignar' : $product->stalls()->where('product_id', $product->id)->name,
+                    "stock" => $status == false ? null : $product->stalls()->where('product_id', $product->id)->sum('product_stall.quantity'),
+                    "price" => $status == false ? null : $product->stalls()->where('product_id', $product->id)->sum('product_stall.price_per_unit')
+                ]);
+            }
+
+            return view('general.products', [$data]);
+        }
+    }
+
+    public function storeProduct(StoreProductRequest $request){
+        if(Auth::user()->hasRole('seller')){
+            $data = $request->validated();
+
+            $product = Product::create([
+                "name" => $data->name,
+                "unit" => $data->unit,
+                "user_id" => $data->user_id,
+                "category_id" => $data->category_id
+            ]);
+
+            $photo = Photo::create([
+                "url" => $data->img,
+                "product_id" => $product->id
+            ]);
+
+            $assigned = false;
+            if($request->filled('stall_id')){
+                $product->stall()->attach($data->stall_id, [
+                    "product_id" => $product->id,
+                    "stall_id" => $data->stall_id,
+                    "quantity" => $data->quantity,
+                    "price_per_unit" => $data->price
+                ]);
+                $assigned = true;
+            }
+
+            return response()->json([
+                "message" => 'Producto insertado con éxito',
+                "data" => [
+                    "product" => $product,
+                    "img" => $photo,
+                    "stalls" => $assigned == false ? null : $product->stall()->where('product_id', $product->id)
+                ]
+            ]);
         }
     }
 }
