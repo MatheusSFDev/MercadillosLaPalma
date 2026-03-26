9b0bad243a6f16a3493178c546774db424d52bcc
ElZorroGG <106108663+ElZorroGG@users.noreply.github.com>
2026-03-26 19:43:06 +0000

    seeders en clase

--- database/seeders/HolydaysSeeder.php
+++ database/seeders/HolydaysSeeder.php
@@ -22,18 +22,145 @@ class HolydaysSeeder extends Seeder
             [
                 'flea_market_id' => 1,
                 'start_date' => now()->addDays(30),
-                'end_date' => now()->addDays(37),
+                'end_date' => now()->addDays(32),
             ],
             [
                 'flea_market_id' => 1,
                 'start_date' => now()->addDays(60),
-                'end_date' => now()->addDays(67),
+                'end_date' => now()->addDays(63),
             ],
             [
-                'flea_market_id' => 1,
+                'flea_market_id' => 2,
                 'start_date' => now()->addDays(90),
-                'end_date' => now()->addDays(97),
+                'end_date' => now()->addDays(91),
             ],
+            [
+                'flea_market_id' => 2,
+                'start_date' => now()->addDays(120),
+                'end_date' => now()->addDays(130),
+            ],
+            [
+                'flea_market_id' => 3,
+                'start_date' => now()->addDays(150),
+                'end_date' => now()->addDays(157),
+            ],
+            [
+                'flea_market_id' => 3,
+                'start_date' => now()->addDays(180),
+                'end_date' => now()->addDays(184),
+            ],
+            [
+                'flea_market_id' => 4,
+                'start_date' => now()->addDays(210),
+                'end_date' => now()->addDays(213),
+            ],
+            [
+                'flea_market_id' => 4,
+                'start_date' => now()->addDays(240),
+                'end_date' => now()->addDays(245),
+            ],
+            [
+                'flea_market_id' => 5,
+                'start_date' => now()->addDays(272),
+                'end_date' => now()->addDays(275),
+            ],
+            [
+                'flea_market_id' => 5,
+                'start_date' => now()->addDays(300),
+                'end_date' => now()->addDays(301),
+            ],
+            [
+                'flea_market_id' => 6,
+                'start_date' => now()->addDays(330),
+                'end_date' => now()->addDays(331),
+            ],
+            [
+                'flea_market_id' => 6,
+                'start_date' => now()->addDays(360),
+                'end_date' => now()->addDays(362),
+            ],
+            [
+                'flea_market_id' => 7,
+                'start_date' => now()->addDays(400),
+                'end_date' => now()->addDays(404),
+            ],
+            [
+                'flea_market_id' => 7,
+                'start_date' => now()->addDays(450),
+                'end_date' => now()->addDays(451),
+            ],
+            [
+                'flea_market_id' => 8,
+                'start_date' => now()->addDays(500),
+                'end_date' => now()->addDays(505),
+            ],
+            [
+                'flea_market_id' => 8,
+                'start_date' => now()->addDays(550),
+                'end_date' => now()->addDays(551),
+            ],
+            [
+                'flea_market_id' => 9,
+                'start_date' => now()->addDays(600),
+                'end_date' => now()->addDays(603),
+            ],
+            [
+                'flea_market_id' => 9,
+                'start_date' => now()->addDays(650),
+                'end_date' => now()->addDays(655),
+            ],
+            [
+                'flea_market_id' => 10,
+                'start_date' => now()->addDays(700),
+                'end_date' => now()->addDays(705),
+            ],
+            [
+                'flea_market_id' => 10,
+                'start_date' => now()->addDays(750),
+                'end_date' => now()->addDays(759),
+            ],
+            [
+                'flea_market_id' => 11,
+                'start_date' => now()->addDays(800),
+                'end_date' => now()->addDays(805),
+            ],
+            [
+                'flea_market_id' => 11,
+                'start_date' => now()->addDays(850),
+                'end_date' => now()->addDays(851),
+            ],
+            [
+                'flea_market_id' => 12,
+                'start_date' => now()->addDays(900),
+                'end_date' => now()->addDays(905),
+            ],
+            [
+                'flea_market_id' => 12,
+                'start_date' => now()->addDays(950),
+                'end_date' => now()->addDays(953),
+            ],
+            [
+                'flea_market_id' => 13,
+                'start_date' => now()->addDays(1000),
+                'end_date' => now()->addDays(1002),
+            ]
+            ,
+            [
+                'flea_market_id' => 13,
+                'start_date' => now()->addDays(200),
+                'end_date' => now()->addDays(201),
+            ],
+            [
+                'flea_market_id' => 14,
+                'start_date' => now()->addDays(220),
+                'end_date' => now()->addDays(223)  ,
+            ],
+            [
+                'flea_market_id' => 14,
+                'start_date' => now()->addDays(300),
+                'end_date' => now()->addDays(3030),
+            ],
+
         ];
 
         foreach ($holydays as $holyday) {
--- database/seeders/StallsSeeders.php
+++ database/seeders/StallsSeeders.php
@@ -14,7 +14,7 @@ class StallsSeeders extends Seeder
     {
         $stalls = [
             [
-                'user_id' => 1,
+                'user_id' => 3,
                 'flea_market_id' => 1,
                 'home_delivery' => 0,
                 'information' => 'Vendo ropa de segunda mano en buen estado',
@@ -26,7 +26,7 @@ class StallsSeeders extends Seeder
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
-                'user_id' => 2,
+                'user_id' => 3,
                 'flea_market_id' => 1,
                 'home_delivery' => 1,
                 'information' => 'Vendo muebles antiguos restaurados',
@@ -38,7 +38,7 @@ class StallsSeeders extends Seeder
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
-                'user_id' => 2,
+                'user_id' => 4,
                 'flea_market_id' => 2,
                 'home_delivery' => 0,
                 'information' => 'Vendo juguetes y juegos de mesa usados',
@@ -50,7 +50,7 @@ class StallsSeeders extends Seeder
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
-                'user_id' => 1,
+                'user_id' => 4,
                 'flea_market_id' => 2,
                 'home_delivery' => 1,
                 'information' => 'Vendo libros de segunda mano en buen estado',
@@ -58,6 +58,102 @@ class StallsSeeders extends Seeder
                 'reset_date' => date('Y-m-d H:i:s', strtotime('+30 days')),
                 'register_date' => date('Y-m-d H:i:s'),
                 'name' => 'Libros Usados',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'user_id' => 6,
+                'flea_market_id' => 1,
+                'home_delivery' => 0,
+                'information' => 'muebles de segunda mano en buen estado',
+                'active' => 1,
+                'reset_date' => date('Y-m-d H:i:s', strtotime('+30 days')),
+                'register_date' => date('Y-m-d H:i:s'),
+                'name' => 'Muebles y más',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'user_id' => 13,
+                'flea_market_id' => 1,
+                'home_delivery' => 1,
+                'information' => 'Vendo juegos retro',
+                'active' => 1,
+                'reset_date' => date('Y-m-d H:i:s', strtotime('+30 days')),
+                'register_date' => date('Y-m-d H:i:s'),
+                'name' => 'Juegos Retro',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'user_id' => 9,
+                'flea_market_id' => 1,
+                'home_delivery' => 0,
+                'information' => 'Floristeria con plantas de interior y exterior',
+                'active' => 1,
+                'reset_date' => date('Y-m-d H:i:s', strtotime('+30 days')),
+                'register_date' => date('Y-m-d H:i:s'),
+                'name' => 'Floristeria',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'user_id' => 4,
+                'flea_market_id' => 1,
+                'home_delivery' => 1,
+                'information' => 'ropa organica y sostenible',
+                'active' => 1,
+                'reset_date' => date('Y-m-d H:i:s', strtotime('+30 days')),
+                'register_date' => date('Y-m-d H:i:s'),
+                'name' => 'Ropa Orgánica',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'user_id' => 6,
+                'flea_market_id' => 1,
+                'home_delivery' => 0,
+                'information' => 'fruta y verdura de temporada',
+                'active' => 1,
+                'reset_date' => date('Y-m-d H:i:s', strtotime('+30 days')),
+                'register_date' => date('Y-m-d H:i:s'),
+                'name' => 'Frutas y Verduras',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'user_id' => 9,
+                'flea_market_id' => 1,
+                'home_delivery' => 1,
+                'information' => 'artículos de decoración para el hogar',
+                'active' => 1,
+                'reset_date' => date('Y-m-d H:i:s', strtotime('+30 days')),
+                'register_date' => date('Y-m-d H:i:s'),
+                'name' => 'Decoración Hogar',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'user_id' => 13,
+                'flea_market_id' => 1,
+                'home_delivery' => 0,
+                'information' => 'artículos de electrónica de segunda mano',
+                'active' => 1,
+                'reset_date' => date('Y-m-d H:i:s', strtotime('+30 days')),
+                'register_date' => date('Y-m-d H:i:s'),
+                'name' => 'Electrónica Usada',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'user_id' => 9,
+                'flea_market_id' => 1,
+                'home_delivery' => 1,
+                'information' => 'artículos de cocina y',
+                'active' => 1,
+                'reset_date' => date('Y-m-d H:i:s', strtotime('+30 days')),
+                'register_date' => date('Y-m-d H:i:s'),
+                'name' => 'Cocina con manolo',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ]
