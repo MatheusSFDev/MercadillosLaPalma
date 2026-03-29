fc0cfb3573934e04afbfe46dc5fa4d8c5407fde1
ElZorroGG <106108663+ElZorroGG@users.noreply.github.com>
2026-03-29 23:08:23 +0200

    Fix Consistencia

--- database/seeders/StallsCategorySeeder.php
+++ database/seeders/StallsCategorySeeder.php
@@ -14,22 +14,70 @@ class StallsCategorySeeder extends Seeder
     public function run(): void
     {
         $stallsCategories = [
-            ['stall_id' => 1, 'category_id' => 1],
-            ['stall_id' => 2, 'category_id' => 3],
-            ['stall_id' => 3, 'category_id' => 6],
-            ['stall_id' => 4, 'category_id' => 4],
-            ['stall_id' => 5, 'category_id' => 2],
-            ['stall_id' => 6, 'category_id' => 5],
-            ['stall_id' => 7, 'category_id' => 7],
-            ['stall_id' => 8, 'category_id' => 8],
-            ['stall_id' => 9, 'category_id' => 8],
-            ['stall_id' => 10, 'category_id' => 3],
-            ['stall_id' => 10, 'category_id' => 8],
-            ['stall_id' => 11, 'category_id' => 8],
-            ['stall_id' => 12, 'category_id' => 3],
-            ['stall_id' => 13, 'category_id' => 4],
-            ['stall_id' => 13, 'category_id' => 8],
-            ['stall_id' => 14, 'category_id' => 3],
+            [
+                'stall_id' => 1,
+                'category_id' => 1,
+            ],
+            [
+                'stall_id' => 2,
+                'category_id' => 3,
+            ],
+            [
+                'stall_id' => 3,
+                'category_id' => 6,
+            ],
+            [
+                'stall_id' => 4,
+                'category_id' => 4,
+            ],
+            [
+                'stall_id' => 5,
+                'category_id' => 2,
+            ],
+            [
+                'stall_id' => 6,
+                'category_id' => 5,
+            ],
+            [
+                'stall_id' => 7,
+                'category_id' => 7,
+            ],
+            [
+                'stall_id' => 8,
+                'category_id' => 8,
+            ],
+            [
+                'stall_id' => 9,
+                'category_id' => 8,
+            ],
+            [
+                'stall_id' => 10,
+                'category_id' => 3,
+            ],
+            [
+                'stall_id' => 10,
+                'category_id' => 8,
+            ],
+            [
+                'stall_id' => 11,
+                'category_id' => 8,
+            ],
+            [
+                'stall_id' => 12,
+                'category_id' => 3,
+            ],
+            [
+                'stall_id' => 13,
+                'category_id' => 4,
+            ],
+            [
+                'stall_id' => 13,
+                'category_id' => 8,
+            ],
+            [
+                'stall_id' => 14,
+                'category_id' => 3,
+            ],
         ];
 
         foreach ($stallsCategories as $stallCategory) {
